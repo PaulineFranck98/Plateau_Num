f5243ed86433baa41b10d1f87268d26aafca7647
PaulineFranck98 <pauline-franck@hotmail.fr>
2023-12-01 16:57:10 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	new file:   php2/exo13.1.php

---
+++ php2/exo13.1.php
@@ -0,0 +1,69 @@
+<?php
+
+class Voiture {
+    private string $_marque;
+    private string $_modele;
+    private int $_nbPortes;
+    private int $_vitesseActuelle;
+
+    public function __construct(string $marque, string $modele, int $nbPortes){
+
+        $this->_marque = $marque;
+        $this->_modele = $modele;
+        $this->_nbPortes = $nbPortes;
+        $this->_vitesseActuelle = 0;
+    }
+
+    public function demarrer(){
+
+    }
+
+    public function accelerer(){
+
+    }
+
+    public function stopper(){
+
+    }
+
+    // getter 
+
+    public function getMarque(){
+        return $this->_marque;
+    }
+
+    public function getModele(){
+        return $this->_modele;
+
+    }
+
+    public function getNbPortes(){
+        return $this->_nbPortes;
+
+    }
+
+    public function getVitesseActuelle(){
+        return $this->_vitesseActuelle;
+    }
+
+
+    // setter
+
+    public function setMarque($marque){
+        $this->_marque = $marque;
+    }
+    
+    public function setModele($modele){
+        $this->_modele = $modele;
+
+    }
+    public function setNbPortes($NbPortes){
+        $this->_NbPortes = $NbPortes;
+    }
+    
+    public function setVitesseActuelle($vitesseActuelle){
+        $this->_vitesseActuelle = $vitesseActuelle;
+    }
+
+
+}
