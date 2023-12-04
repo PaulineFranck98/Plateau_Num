fb37e1866151dd0e0b41e08a4aa754eafae7f1f6
PaulineFranck98 <pauline-franck@hotmail.fr>
2023-12-04 16:57:22 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   php2/exo14.1.php

--- php2/exo14.1.php
+++ php2/exo14.1.php
@@ -38,41 +38,56 @@ class Voiture {
     }
 
     public function getInfos(){
-        return 
 
+        return "Infos véhicule : <br>" .
+        "Marque du véhicule : " . $this->marque . "<br>" .
+        "Modèle du véhicule : " . $this->modele . "<br>" ;
     }
     
 }
 
 
 class VoitureElec extends Voiture {
-
+    
     private int $autonomie;
-
+    
     public function __construct($marque, $modele, $autonomie){
-
+        
         // oblige la classe VoitureElec à respecter  les paramètres du constructeur de sa classe mère
         parent::__construct($marque , $modele);
-
+        
         $this->autonomie = $autonomie;
-
+        
     }
-
+    
     public function getAutonomie(){
         return $this->autonomie;
     }
-
+    
     public function setAutonomie($autonomie){
         $this->autonomie = $autonomie;
     }
-
-
-}
+    
+    public function getInfos(){
         
+        // concaténation juste après la fonction 
+         return parent::getInfos() . "Autonomie : " . $this->autonomie . "<br>";
+         
+        // autre option :  
+        // "Infos véhicule : <br>" .
+        // "Marque du véhicule : " . $this->marque . "<br>" .
+        // "Modèle du véhicule : " . $this->modele . "<br>" .
+    }
+    
+}
+
+
 
 $v1 = new Voiture ("Peugeot", "408");
+echo $v1->getInfos() . "<br>";
 
 $ve1 = new VoitureElec ("BMW", "I3", 100);
+echo $ve1->getInfos();
 
-var_dump($v1);
-var_dump($ve1);
+// var_dump($v1);
+// var_dump($ve1);
